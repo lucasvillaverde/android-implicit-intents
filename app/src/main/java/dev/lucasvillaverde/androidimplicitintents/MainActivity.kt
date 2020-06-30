542ee5d71e7eaa105a110f6561eb2d6b8adf7d4e
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-29 23:15:16 -0300

    Added new fragments and circle dots to paging the viewpager2

--- app/src/main/java/dev/lucasvillaverde/androidimplicitintents/MainActivity.kt
+++ app/src/main/java/dev/lucasvillaverde/androidimplicitintents/MainActivity.kt
@@ -5,34 +5,30 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2.ORIENTATION_HORIZONTAL
-import dev.lucasvillaverde.androidimplicitintents.examples.ImageIntentFragment
-import dev.lucasvillaverde.androidimplicitintents.examples.IntentTypesAdapter
-import dev.lucasvillaverde.androidimplicitintents.examples.TextIntentFragment
+import dev.lucasvillaverde.androidimplicitintents.examples.*
 import kotlinx.android.synthetic.main.activity_main.*
+import me.relex.circleindicator.CircleIndicator3
 
 class MainActivity : AppCompatActivity() {
 
     private val intentTypesAdapter = IntentTypesAdapter(supportFragmentManager, lifecycle)
-    private val imageFragment = ImageIntentFragment()
-    private val textFragment = TextIntentFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        intentTypesAdapter.addFragment(imageFragment)
-        intentTypesAdapter.addFragment(textFragment)
+        addFragmentsToAdapter()
         intentViewPager.adapter = intentTypesAdapter
         intentViewPager.orientation = ORIENTATION_HORIZONTAL
 
+        // Enabling and connecting the circle dots to our viewpager.
+        indicator.setViewPager(intentViewPager)
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Message Intent
-
-
-        //
+    private fun addFragmentsToAdapter() {
+        intentTypesAdapter.addFragment(ImageIntentFragment())
+        intentTypesAdapter.addFragment(TextIntentFragment())
+        intentTypesAdapter.addFragment(WebIntentFragment())
+        intentTypesAdapter.addFragment(MapIntentFragment())
     }
 }
