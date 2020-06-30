156a660664b3a41f05f60a6ea23281ca03c9dfde
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-29 23:16:20 -0300

    Changed TextIntentFragment, removed result

--- app/src/main/java/dev/lucasvillaverde/androidimplicitintents/examples/TextIntentFragment.kt
+++ app/src/main/java/dev/lucasvillaverde/androidimplicitintents/examples/TextIntentFragment.kt
@@ -1,18 +1,17 @@
 package dev.lucasvillaverde.androidimplicitintents.examples
 
 import android.content.Intent
-import android.content.Intent.ACTION_SEND
-import android.content.Intent.EXTRA_TEXT
+import android.content.Intent.*
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import dev.lucasvillaverde.androidimplicitintents.Constants
 import dev.lucasvillaverde.androidimplicitintents.R
-import kotlinx.android.synthetic.main.fragment_image_intent.*
 import kotlinx.android.synthetic.main.fragment_text_intent.*
 
 class TextIntentFragment : Fragment() {
@@ -26,20 +25,14 @@ class TextIntentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btnSendText.setOnClickListener {
+
             val intent = Intent().apply {
-                action = ACTION_SEND
-                type = "text/plain"
-                putExtra(EXTRA_TEXT, edtSendText.text)
+                action = ACTION_VIEW
+                type = "vnd.android-dir/mms-sms"
+                putExtra("sms_body", "${edtSendText.text}")
             }
 
-            startActivityForResult(intent, Constants.REQUEST_SEND_TEXT)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == Constants.REQUEST_SEND_TEXT) {
-            Toast.makeText(activity, "Text sent!", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
         }
     }
 
